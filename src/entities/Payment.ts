4cd5d2edd17a1a26fae5dec9e2357cf421a110b3
Simón Alberto Godoy González <simon.godoy@placetopay.com>
2020-04-10 13:57:33 -0500

    Add payment array filter to object.

--- src/entities/Payment.ts
+++ src/entities/Payment.ts
@@ -71,11 +71,17 @@ export class Payment extends Entity {
     super();
 
     this.reference = data.hasOwnProperty("reference") ? data.reference : null;
-    this.description = data.hasOwnProperty("description") ? data.description : null;
-    this.allowPartial = data.hasOwnProperty("allowPartial") ? data.allowPartial : null;
+    this.description = data.hasOwnProperty("description")
+      ? data.description
+      : null;
+    this.allowPartial = data.hasOwnProperty("allowPartial")
+      ? data.allowPartial
+      : null;
     this.subscribe = data.hasOwnProperty("subscribe") ? data.subscribe : null;
     this.agreement = data.hasOwnProperty("agreement") ? data.agreement : null;
-    this.agreementType = data.hasOwnProperty("agreementType") ? data.agreementType : null;
+    this.agreementType = data.hasOwnProperty("agreementType")
+      ? data.agreementType
+      : null;
     this.amount = data.hasOwnProperty("amount") ? data.amount : null;
     this.recurring = data.hasOwnProperty("recurring") ? data.recurring : null;
     this.shipping = data.hasOwnProperty("shipping") ? data.shipping : null;
@@ -84,9 +90,99 @@ export class Payment extends Entity {
   }
 
   /**
+   * @returns {string}
+   */
+  getReference(): string {
+    return this.reference;
+  }
+
+  /**
+   * @returns {string}
+   */
+  getDescription(): string {
+    return this.description;
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  getAllowPartial(): boolean {
+    return this.allowPartial;
+  }
+
+  /**
+   * @returns {boolean}
+   */
+  getSubscribe(): boolean {
+    return this.subscribe;
+  }
+
+  /**
+   * @returns {string}
+   */
+  getAgreement(): string {
+    return this.agreement;
+  }
+
+  /**
+   * @returns {string}
+   */
+  getAgreementType(): string {
+    return this.agreementType;
+  }
+
+  /**
+   * @returns {any}
+   */
+  getAmount(): any {
+    return this.amount;
+  }
+
+  /**
+   * @returns {any}
+   */
+  getRecurring(): any {
+    return this.recurring;
+  }
+
+  /**
+   * @returns {any}
+   */
+  getShipping(): any {
+    return this.shipping;
+  }
+
+  /**
+   * @returns {any}
+   */
+  getItems(): any {
+    return this.items;
+  }
+
+  /**
+   * @returns {any}
+   */
+  getGds(): any {
+    return this.gds;
+  }
+
+  /**
    * @returns {any}
    */
   toObject(): any {
-    return {};
+    return this.arrayFilter({
+      reference: this.getReference(),
+      description: this.getDescription(),
+      amount: this.getAmount(),
+      allowPartial: this.getAllowPartial(),
+      shipping: this.getShipping(),
+      items: this.getItems(),
+      recurring: this.getRecurring(),
+      subscribe: this.getSubscribe(),
+      additional: null, // This is in EntityWithNameValuePair
+      agreement: this.getAgreement(),
+      agreementType: this.getAgreementType(),
+      gds: this.getGds(),
+    });
   }
 }
